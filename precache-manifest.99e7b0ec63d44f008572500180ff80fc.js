3ae1e8ee4a8d21bfe3c3ce7132ca8ed1cbeeaa4d
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2020-08-20 11:28:07 +0100

    Updates

---
+++ precache-manifest.99e7b0ec63d44f008572500180ff80fc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a6a38bb8802cd7956eb1cd96f3a618",
+    "url": "/pokemon-app-react-tailwind/index.html"
+  },
+  {
+    "revision": "f375a9cb600a9f8bf1fc",
+    "url": "/pokemon-app-react-tailwind/static/css/main.2f169b41.chunk.css"
+  },
+  {
+    "revision": "99bb6ff9ab411c7052ad",
+    "url": "/pokemon-app-react-tailwind/static/js/2.03094d5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-app-react-tailwind/static/js/2.03094d5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f375a9cb600a9f8bf1fc",
+    "url": "/pokemon-app-react-tailwind/static/js/main.a132fc23.chunk.js"
+  },
+  {
+    "revision": "eba9ef54b044c42b9505",
+    "url": "/pokemon-app-react-tailwind/static/js/runtime-main.4bc5f420.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokemon-app-react-tailwind/static/media/Pokemon Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "6477dbbbaed5dc0970f8c231f9614995",
+    "url": "/pokemon-app-react-tailwind/static/media/back.6477dbbb.svg"
+  },
+  {
+    "revision": "619e12c71983ed2066fc4ce1c59aae14",
+    "url": "/pokemon-app-react-tailwind/static/media/background.619e12c7.jpg"
+  },
+  {
+    "revision": "42b9833865b7bf3c3660fac09619d85a",
+    "url": "/pokemon-app-react-tailwind/static/media/next.42b98338.svg"
+  },
+  {
+    "revision": "b18f6f3bfd7e9921c88edc868ed3a75f",
+    "url": "/pokemon-app-react-tailwind/static/media/pokemon.b18f6f3b.svg"
+  },
+  {
+    "revision": "45a0ba998010121c05fe89dea34a71ea",
+    "url": "/pokemon-app-react-tailwind/static/media/previous.45a0ba99.svg"
+  }
+]);
